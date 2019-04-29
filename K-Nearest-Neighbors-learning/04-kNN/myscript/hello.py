884806a7c527ccc0f4c37ce884b97b4f190a7eae
李威 <121088825@qq.com>
2019-04-29 11:09:25 +0800

    添加了参考书里的代码。

--- K-Nearest-Neighbors-learning/04-kNN/myscript/hello.py
+++
@@ -1,3 +0,0 @@
-def say(name):
-    print('my name is {}.'.format(name))
-print("hello")
---
+++ Reference-Books/黄永昌《scikit-learn 机器学习》/common/utils.py
@@ -0,0 +1,100 @@
+from sklearn.model_selection import learning_curve
+import numpy as np
+
+
+def plot_learning_curve(plt, estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and training learning curve.
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - An object to be used as a cross-validation generator.
+          - An iterable yielding train/test splits.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : integer, optional
+        Number of jobs to run in parallel (default 1).
+    """
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o--', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+# 将交叉验证的结果可视化
+# 根据交叉验证的结果绘制参数的曲线图
+# 注意：这里不是学习曲线，跟训练样本的数据多少是没有关系的
+
+def plot_param_curve(plt, train_sizes, cv_results, xlabel):
+    # 训练数据集的平均值和标准差
+    train_scores_mean = cv_results['mean_train_score']
+    train_scores_std = cv_results['std_train_score']
+    # 测试数据集的平均值和标准差
+    test_scores_mean = cv_results['mean_test_score']
+    test_scores_std = cv_results['std_test_score']
+    plt.title('parameters turning')
+    plt.grid()
+    plt.xlabel(xlabel)
+    plt.ylabel('score')
+
+    # 绘制一个阴影区域
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    # 绘制一个阴影区域
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1,
+                     color="g")
+    plt.plot(train_sizes, train_scores_mean, '.--', color="r", label="Training score")
+    plt.plot(train_sizes, test_scores_mean, '.-', color="g", label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
